d27b9a4309493fa977cc3203b6cb4308af188dde
onursabanoglu <>
2022-02-08 15:15:28 +0300

    create scss architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+
+
 gulp.task('sass', async function() {
-  gulp.src('scss/main.scss')
+  gulp.src('scss/globals/styles/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('./dist/css'));
 });
