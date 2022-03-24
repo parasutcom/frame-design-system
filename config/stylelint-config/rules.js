76c77387bf5827bac18bdd37184647e3d1c14666
onursabanoglu <>
2022-03-24 12:13:29 +0300

    Update stylelint config file

--- config/stylelint-config/rules.js
+++ config/stylelint-config/rules.js
@@ -1,16 +1,34 @@
 module.exports = {
   rules: {
+    'color-hex-case': 'lower',
+    'color-named': [
+      'never',
+      {
+        ignore: ['inside-function'],
+      },
+    ],
+    'color-no-invalid-hex': true,
+    'selector-max-id': 1,
+    'selector-no-qualifying-type': [
+      true,
+      {
+        ignore: ['attribute'],
+      },
+    ],
+    'no-duplicate-selectors': true,
+    'at-rule-name-space-after': 'always-single-line',
+    'declaration-block-single-line-max-declarations': 1,
+    'unit-no-unknown': true,
     'property-no-unknown': true,
     'block-no-empty': true,
     'comment-no-empty': true,
-    'no-duplicate-selectors': true,
-    'color-named': 'never',
+
     'length-zero-no-unit': true,
     'declaration-no-important': true,
     'string-quotes': 'single',
     'font-weight-notation': 'numeric',
     'selector-class-pattern': null,
-    'font-family-name-quotes': 'always-unless-keyword',
+    'font-family-name-quotes': 'always-where-recommended',
     'max-line-length': [
       80,
       {
