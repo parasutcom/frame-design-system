3545d79b7a8323e027b65b45b4bc97fc397c74d6
onursabanoglu <>
2022-03-24 12:48:57 +0300

    Update stylelint

--- config/stylelint-config/index.js
+++ config/stylelint-config/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 module.exports = {
   extends: [
-    'stylelint-config-standard-scss',
-    'stylelint-config-idiomatic-order',
-    './rules',
-    './plugins/scss',
-    './plugins/prettier',
+    require.resolve('stylelint-config-standard-scss'),
+    require.resolve('stylelint-config-idiomatic-order'),
+    require.resolve('./rules/rules'),
+    require.resolve('./plugins/scss'),
+    require.resolve('./plugins/prettier'),
   ],
 };
