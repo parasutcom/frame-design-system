b582e4d4a2221184459d80c3dbbe1bafece7d0b6
onursabanoglu <onursabanoglu0@gmail.com>
2022-06-09 13:09:34 +0300

    Update gulp.js and package.json scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,46 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 
+const buildStyles = () => {
+  return gulp
+    .src('src/scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/css/'));
+};
 
+const buildStylesMinify = () => {
+  return gulp
+    .src('src/scss/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./dist/css/'));
+};
 
-gulp.task('sass', async function() {
-  gulp.src('src/scss/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist/css'));
-});
+const browsersync = () => {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+    startPath: './html/index.html',
+  });
+};
 
+const watch = () => {
+  gulp.watch(['src/**/*.scss'], gulp.series(buildStyles, buildStylesMinify));
+  gulp.watch(['dist/css/main.css']).on('change', browserSync.reload);
+};
 
-gulp.task('watch', function() {
-  gulp.watch('./src/scss/*.scss', gulp.series('sass'));
-})
+gulp.task(
+  'default',
+  gulp.series(
+    gulp.parallel(buildStyles, buildStylesMinify),
+    gulp.parallel(browsersync, watch),
+  ),
+);
