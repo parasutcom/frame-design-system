1cf0e2355bbe32e8284fb343d067c2e2a5911071
onursabanoglu <onursabanoglu0@gmail.com>
2022-10-10 10:42:51 +0300

    refactor: change file structure

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const sourcemaps = require('gulp-sourcemaps');
 
 const buildStyles = () => {
   return gulp
-    .src('src/scss/main.scss')
+    .src('src/styles/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
@@ -17,7 +17,7 @@ const buildStyles = () => {
 
 const buildStylesMinify = () => {
   return gulp
-    .src('src/scss/main.scss')
+    .src('src/styles/main.scss')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('./dist/css/'));
