369ab04cbd0ff85b7e917e304411a042b942f601
onursabanoglu <onursabanoglu0@gmail.com>
2022-10-21 13:04:57 +0300

    feat(ci): add husky and configure commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-exclamation-mark': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100],
+  },
+}
