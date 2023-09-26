54a8ecca85c776856699248cd71d8cb2c14bc146
Onur Şabanoğlu <onursabanoglu0@gmail.com>
2023-09-26 16:42:18 +0300

    chore: edit commitlint config file

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'header-case': [2, 'always', 'lower-case'],
+    'header-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
-    'subject-exclamation-mark': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100],
+    'subject-exclamation-mark': [2, 'never'],
   },
 };
