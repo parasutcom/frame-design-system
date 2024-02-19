a6c212be2b61c1f7700ee3c3bc2cc0116f3e8574
Onur Şabanoğlu <onursabanoglu0@gmail.com>
2024-02-19 16:59:40 +0300

    feat(tooltip): add custom js for interactive tooltip

--- website/src/components/ComponentInteractions.js
+++ website/src/components/ComponentInteractions.js
@@ -5,7 +5,24 @@ function ComponentInteractions() {
   const location = useLocation();
 
   useEffect(() => {
-    const applyComponentActions = () => {};
+    const applyComponentActions = () => {
+      setTimeout(function () {
+        const tooltip = document.querySelectorAll('.fds-tooltip');
+        tooltip.forEach((tooltip) => {
+          tooltip.addEventListener('mouseenter', () => {
+            if (tooltip) {
+              tooltip.classList.add('fds-tooltip--open');
+            }
+          });
+
+          tooltip.addEventListener('mouseleave', () => {
+            if (tooltip) {
+              tooltip.classList.remove('fds-tooltip--open');
+            }
+          });
+        });
+      }, 1000);
+    };
     applyComponentActions();
   }, [location.pathname]);
   return null;
