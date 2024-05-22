47d359cdb6e7d7d7c3a7151e7f6c42f1985f9e15
Onur Şabanoğlu <47773417+onursabanoglu@users.noreply.github.com>
2024-05-22 17:10:15 +0300

    feat(typescript): add typescript (#139)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-}
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
