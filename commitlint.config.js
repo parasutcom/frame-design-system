02b610248a7072cb1ccc2434ad3c15e4e718231e
Onur Şabanoğlu <onursabanoglu0@gmail.com>
2024-05-24 10:12:47 +0300

    chore(commitlint): delete rules at commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-case': [2, 'always', 'lower-case'],
-    'header-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
-    'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'subject-exclamation-mark': [2, 'never'],
-  },
 };
