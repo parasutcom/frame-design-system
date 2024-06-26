11e7d21c1c2fb34a4983e25c25ad1621da32caaa
Onur Şabanoğlu <onursabanoglu0@gmail.com>
2024-06-26 09:50:25 +0300

    feat(storybook): add storybook for component development and documentation

---
+++ .storybook/fds-theme.js
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming/create';
+ 
+export default create({
+  base: 'light',
+  brandTitle: 'Frame Design system',
+  brandUrl: 'https://framedesignsystem.com',
+  brandTarget: '_target',
+
+  colorPrimary: '#0F62FE',
+  textColor: '#1F1F1F',
+});
---
+++ .storybook/main.ts
@@ -0,0 +1,66 @@
+import type { StorybookConfig } from '@storybook/web-components-webpack5';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../docs/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-webpack5-compiler-swc',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/web-components-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+  staticDirs: [
+    '../dist',
+  ],
+  webpackFinal: async (config) => {
+    if (!config.module) {
+      config.module = { rules: [] };
+    }
+    
+    if (!config.module.rules) {
+      config.module.rules = [];
+    }
+
+    config.module.rules.push(
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css|\.s(c|a)ss$/,
+        use: [
+          {
+            loader: 'lit-scss-loader',
+            options: {
+              minify: true,
+            },
+          },
+          'extract-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    );
+
+    if (!config.resolve) {
+      config.resolve = { extensions: [] };
+    }
+    
+    if (!config.resolve.extensions) {
+      config.resolve.extensions = [];
+    }
+
+    config.resolve.extensions.push('.ts', '.tsx');
+    
+    return config;
+  },
+};
+export default config;
