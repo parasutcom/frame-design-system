3409066e9ea5de14edc3b277a73d6ad860ac522a
Onur Şabanoğlu <47773417+onursabanoglu@users.noreply.github.com>
2024-07-18 07:30:40 +0300

    feat(spinner): add fds-spinner component (#163)

---
+++ src/components/spinner/fds-spinner.stories.ts
@@ -0,0 +1,103 @@
+import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+import './fds-spinner';
+import '../icon/fds-icon';
+
+export default {
+  title: 'Components/Spinner',
+  component: 'fds-spinner',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'A spinner is an animated circle icon used to indicate an indeterminate loading process.',
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    size: {
+      options: ['small', 'medium'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+    label: {
+      control: {
+        type: 'text',
+      },
+    },
+    LabelPlacement: {
+      options: ['horizontal', 'vertical'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+  args: {
+    variant: 'primary',
+    size: 'medium',
+  },
+};
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const Base = (args: any) => html`
+  <fds-spinner
+    variant=${ifDefined(args.variant)}
+    size=${ifDefined(args.size)}
+    label=${ifDefined(args.label)}
+    label-placement=${ifDefined(args.LabelPlacement)}
+  ></fds-spinner>
+`;
+
+export const Variants = {
+  render: () => html`
+    <div style="display: flex; align-items: center; gap: 1rem">
+      <fds-spinner variant="primary"></fds-spinner>
+      <fds-spinner variant="secondary"></fds-spinner>
+    </div>
+  `,
+};
+
+export const Size = {
+  render: () => html`
+    <div style="display: flex; flex-direction: column; gap: 1rem">
+      <div style="display: flex; align-items: center; gap: 1rem">
+        <fds-spinner variant="primary" size="medium"></fds-spinner>
+        <fds-spinner variant="primary" size="small"></fds-spinner>
+      </div>
+      <div style="display: flex; align-items: center; gap: 1rem">
+        <fds-spinner variant="secondary" size="medium"></fds-spinner>
+        <fds-spinner variant="secondary" size="small"></fds-spinner>
+      </div>
+    </div>
+  `,
+};
+
+export const WithLabel = {
+  render: () => html`
+    <div style="display: flex; align-items: center; gap: 1rem">
+      <fds-spinner variant="primary" label="Spinner label"></fds-spinner>
+      <fds-spinner variant="secondary" label="Spinner label"></fds-spinner>
+    </div>
+  `,
+};
+
+export const LabelPlacement = {
+  render: () => html`
+    <div style="display: flex; align-items: center; gap: 1rem">
+      <fds-spinner
+        variant="primary"
+        label="Spinner label"
+        label-placement="horizontal"
+      ></fds-spinner>
+      <fds-spinner variant="primary" label="Spinner label" label-placement="vertical"></fds-spinner>
+    </div>
+  `,
+};
---
+++ src/components/spinner/fds-spinner.ts
@@ -0,0 +1,40 @@
+import { html, LitElement, TemplateResult, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+import style from './fds-spinner.scss';
+
+export type SpinnerVariant = 'primary' | 'secondary';
+export type SpinnerSize = 'small' | 'medium';
+export type LabelPlacement = 'horizontal' | 'vertical';
+
+@customElement('fds-spinner')
+export class FdsSpinner extends LitElement {
+  static get styles() {
+    return [unsafeCSS(style)];
+  }
+
+  @property({ type: String, reflect: true })
+  variant: SpinnerVariant = 'primary';
+
+  @property({ type: String, reflect: true })
+  size: SpinnerSize = 'medium';
+
+  @property({ type: String, reflect: true })
+  label?: string = '';
+
+  @property({ type: String, reflect: true, attribute: 'label-placement' })
+  LabelPlacement: LabelPlacement = 'horizontal';
+
+  render(): TemplateResult {
+    const iconSize = this.size === 'medium' ? '3x-large' : 'medium';
+    return html`<div
+      class="fds-spinner"
+      role="progressbar"
+      aria-label="${ifDefined(this.label ? this.label : 'Loading')}"
+    >
+      <fds-icon prefix="far" name="spinner-third" size="${iconSize}"></fds-icon>
+      ${this.label ? html`<div class="fds-spinner__label">${this.label}</div>` : ''}
+    </div>`;
+  }
+}
