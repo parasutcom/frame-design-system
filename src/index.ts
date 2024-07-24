90b7e2a77c573c679dfd8bcfe573ad85cc42fce5
Onur Şabanoğlu <onursabanoglu0@gmail.com>
2024-07-24 18:35:16 +0300

    feat(toggle-switch): add toggle-switch-component

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { FdsButton } from './components/button/fds-button';
 export { FdsIcon } from './components/icon/fds-icon';
 export { FdsProgressBar } from './components/progress-bar/fds-progress-bar';
 export { FdsSpinner } from './components/spinner/fds-spinner';
+export { FdsToggleSwitch } from './components/toggle-switch/fds-toggle-switch';
