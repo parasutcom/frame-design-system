50cf7c8e2965972e9d87b9caaba6158202d8e038
Onur Şabanoğlu <47773417+onursabanoglu@users.noreply.github.com>
2024-10-22 22:11:04 +0300

    chore(storybook): customize storybook FDS theme (#184)

--- .storybook/fds-theme.js
+++ .storybook/fds-theme.js
@@ -2,10 +2,15 @@ import { create } from '@storybook/theming/create';
  
 export default create({
   base: 'light',
-  brandTitle: 'Frame Design system',
+  fontBase: '"Inter", sans-serif',
+  fontCode: 'monospace',
+  brandTitle: 'Frame Design System',
   brandUrl: 'https://framedesignsystem.com',
-  brandTarget: '_target',
-
+  brandImage: '/images/logo.svg',
+  brandTarget: '_self',
   colorPrimary: '#0F62FE',
-  textColor: '#1F1F1F',
+  colorSecondary: '#202E49',
+  textColor: '#141819',
+  appBg: '#F7F9FA',
+  barBg: '#F7F9FA',
 });
